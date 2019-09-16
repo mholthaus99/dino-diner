f34b00511a521f3b51c84b45698d4cd48b487955
unknown <mholthaus99@gmail.com>
2019-09-15 23:12:06 -0500

    All menu classes built and working properly

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public int NuggetCount { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { NuggetCount + 
+                    " Chicken Nuggets"};
+
+                return ingredients;
+            }
+        }
+
+        public void AddNugget()
+        {
+            NuggetCount++;
+            Calories += 59;
+            Price += 0.25;
+        }
+
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 6 * 59;
+            this.NuggetCount = 6;
+        }
+    }
+}
---
+++ Menu/Entrees/TRexKingBurger.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class TRexKingBurger
+    {
+        private bool bun = true;
+        private bool lettuce = true;
+        private bool tomato = true;
+        private bool onion = true;
+        private bool pickle = true;
+        private bool ketchup = true;
+        private bool mustard = true;
+        private bool mayo = true;
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>() { "3 Steakburger Patties" };
+                if (bun) ingredients.Add("Whole-wheat bun");
+                if (pickle) ingredients.Add("Pickle");
+                if (ketchup) ingredients.Add("Ketchup");
+                if (mustard) ingredients.Add("Mustard");
+                if (lettuce) ingredients.Add("Lettuce");
+                if (tomato) ingredients.Add("Tomato");
+                if (onion) ingredients.Add("Onion");
+                if (mayo) ingredients.Add("Mayo");
+
+                return ingredients;
+            }
+        }
+
+        public TRexKingBurger()
+        {
+            this.Price = 8.45;
+            this.Calories = 728;
+        }
+
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+
+        public void HoldPickle()
+        {
+            this.bun = false;
+        }
+        public void HoldKetchup()
+        {
+            this.bun = false;
+        }
+        public void HoldMustard()
+        {
+            this.mustard = false;
+        }
+
+        public void HoldLettuce()
+        {
+            this.lettuce = false;
+        }
+
+        public void HoldTomato()
+        {
+            this.tomato = false;
+        }
+
+        public void HoldOnion()
+        {
+            this.onion = false;
+        }
+
+        public void HoldMayo()
+        {
+            this.mayo = false;
+        }
+    }
+}
